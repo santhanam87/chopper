1e60418c7a36790386b06465f8dea04168d2b325
Santhanam Elumalai <selumalai@opentable.com>
2022-09-09 21:32:23 -0600

    express server integration

--- src/index.test.js
+++
@@ -1,8 +0,0 @@
-import testMethod from '.';
-
-describe('Testing jest integration', () => {
-    it('should return test method', async () => {
-        const returnValue = await testMethod();
-        expect(returnValue).toBe('return value');
-    });
-});
