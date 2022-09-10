1439e81fffc1bf8a222ac00073d27109933d56aa
Santhanam Elumalai <selumalai@opentable.com>
2022-09-09 22:03:17 -0600

    webpact client build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,58 @@ const path = require('path');
 const NodeExternals = require('webpack-node-externals');
 
 module.exports = () => [
-	{
-		entry: './src/index',
-		mode: 'development',
-		target: 'node',
-		externals: [NodeExternals()],
-		cache: false,
-		devtool: 'source-map',
-		output: {
-			filename: 'server.js',
-			path: path.resolve(__dirname, 'dist'),
-		},
-		watchOptions: {
-			ignored: /node_modules/,
-		},
-		resolve: {
-			extensions: ['.wasm', '.mjs', '.js', '.json', '.dust'],
-			modules: ['node_modules'],
-		},
-		module: {
-			rules: [
-				{
-					test: /\.m?js$/,
-					exclude: /(node_modules)/,
-					resolve: {
-						fullySpecified: false,
-					},
-					use: [
-						{
-							loader: 'swc-loader',
-						},
-					],
-				},
-			],
-		},
-	},
+    {
+        entry: { browser: './src/client/index.js' },
+        mode: 'development',
+        output: {
+            filename: 'pageScript.js',
+            path: path.resolve(__dirname, 'dist/public'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'swc-loader',
+                    },
+                },
+            ],
+        },
+    },
+    {
+        entry: './src/index',
+        mode: 'development',
+        target: 'node',
+        externals: [NodeExternals()],
+        cache: false,
+        devtool: 'source-map',
+        output: {
+            filename: 'server.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        watchOptions: {
+            ignored: /node_modules/,
+        },
+        resolve: {
+            extensions: ['.wasm', '.mjs', '.js', '.json', '.dust'],
+            modules: ['node_modules'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules)/,
+                    resolve: {
+                        fullySpecified: false,
+                    },
+                    use: [
+                        {
+                            loader: 'swc-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+    },
 ];
